9ad2c9f602cab40595161479b43e80a028c60d20
mburakergenc <muhammetergenc@gmail.com>
2019-04-11 15:43:13 +0300

    Comments added to the solutions.

--- AddBinary.py
+++ AddBinary.py
@@ -1,14 +1,23 @@
 def addBinary(a, b):
+    # Turn strings into integers
     a = int(a, 2)
     b = int(b, 2)
+    # if a and b is zero return 0 > 0+0=0
     if a == 0 and b == 0:
         return "0"
+    # Since we now have integers, we can sum them up.
     n = a + b
+    # Create empty strings to fill up later on
     strng = ""
     result = ""
+    # Calculate the binary value of n
     while n:
+        # Calculate the remainder of n % 2 and cast it to string
         remainder = str(n % 2)
+        # Add remainder to the string variable
         strng += remainder
+        # Divide n by 2
         n = n // 2
+        # Take the last element in string variable and add it to result
         result = strng[::-1]
     return result
--- PascalTriangle.py
+++ PascalTriangle.py
@@ -1,16 +1,27 @@
 def generate(n):
+    # If n = 0 return an empty array
     if n == 0:
         return []
+    # Create a triangle array (This is the main(outer) array)
     triangle = []
+    # First row will ony have 1 in it.
     first_row = [1]
+    # Append the first row into the triangle array
     triangle.append(first_row)
+    # Create a loop starting from one up to n, since we already have the first row
     for i in range(1, n):
+        # Get the previous row from the triangle array
         prev_row = triangle[i-1]
+        # Create the current row
         cur_row = []
+        # Add 1 at the beginning of the current row
         cur_row.append(1)
+        # Loop through items in the previous row, add them up then append them into the current row
         for j in range(1, i):
             cur_row.append(prev_row[j-1] + prev_row[j])
+        # Append 1 at the end of the current row
         cur_row.append(1)
+        # Append current row to the triangle
         triangle.append(cur_row)
     return triangle
 
